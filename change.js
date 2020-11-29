c84b654844718b3bff38139dead77bfb68b877a8
BeginnerCoder21 <60263821+BeginnerCoder21@users.noreply.github.com>
2020-11-29 18:34:28 +0530

    Add files via upload

---
+++ change.js
@@ -0,0 +1,135 @@
+function random()
+{
+
+  var no=Math.floor(Math.random() * 10);
+  if(no==1)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #1bb5c0;text-align: center;");
+    l.innerHTML="“ Don’t let yesterday take up too much of today ”";
+    document.body.style.backgroundColor = "#1bb5c0";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #1bb5c0");
+    a.innerHTML=" ― Will Rogers ";
+    document.body.style.backgroundColor = " #1bb5c0";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #1bb5c0;");
+  }
+  if(no==2)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #28c84d;text-align: center;");
+    l.innerHTML="“ The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty ”"
+    document.body.style.backgroundColor = "#28c84d";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #28c84d");
+    a.innerHTML=" ― Winston Churchill ";
+    document.body.style.backgroundColor = " #28c84d";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #28c84d;");
+  }
+  if(no==3)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #000573;text-align: center;");
+    l.innerHTML="“ You learn more from failure than from success. Don’t let it stop you. Failure builds character ”";
+    document.body.style.backgroundColor = "#000573";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #000573");
+    a.innerHTML=" ― Unknown ";
+    document.body.style.backgroundColor = " #000573";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #000573;");
+  }
+  if(no==4)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #380045;text-align: center;");
+    l.innerHTML="“ If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you ”";
+    document.body.style.backgroundColor = "#380045";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #380045");
+    a.innerHTML=" ― Steve Jobs ";
+    document.body.style.backgroundColor = " #380045";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #380045;");
+  }
+  if(no==5)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #8000ff ;text-align: center;");
+    l.innerHTML="“ Two things are infinite: the universe and human stupidity; and I'm not sure about the universe ”";
+    document.body.style.backgroundColor = " #8000ff ";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #8000ff");
+    a.innerHTML=" ― Albert Einstein ";
+    document.body.style.backgroundColor = " #8000ff ";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #8000ff ;");
+  }
+  if(no==6)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #74cdc3 ;text-align: center;");
+    l.innerHTML="“ I am so clever that sometimes I don't understand a single word of what I am saying ”";
+    document.body.style.backgroundColor = " #74cdc3 ";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #74cdc3");
+    a.innerHTML=" ― Oscar Wilde ";
+    document.body.style.backgroundColor = " #74cdc3";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #74cdc3;");
+  }
+  if(no==7)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #a200c0 ;text-align: center;");
+    l.innerHTML="“ The whole secret of a successful life is to find out what is one’s destiny to do, and then do it ”";
+    document.body.style.backgroundColor = " #a200c0 ";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #a200c0");
+    a.innerHTML=" ― Henry Ford ";
+    document.body.style.backgroundColor = " #a200c0";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #a200c0;");
+  }
+  if(no==8)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #000000 ;text-align: center;");
+    l.innerHTML="“ When we do the best we can, we never know what miracle is wrought in our life or the life of another. ”";
+    document.body.style.backgroundColor = " #000000 ";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #000000");
+    a.innerHTML=" ― Helen Keller ";
+    document.body.style.backgroundColor = " #000000";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #000000;");
+  }
+  if(no==9)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #791111 ;text-align: center;");
+    l.innerHTML="“ Keep smiling, because life is a beautiful thing and there’s so much to smile about ”";
+    document.body.style.backgroundColor = " #791111 ";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #791111");
+    a.innerHTML=" ― Marilyn Monroe ";
+    document.body.style.backgroundColor = " #791111";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #791111;");
+  }
+  if(no==10)
+  {
+    var l=document.getElementById("label");
+    l.setAttribute("style", "color: #00ff76 ;text-align: center;");
+    l.innerHTML="“ Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring ”";
+    document.body.style.backgroundColor = " #00ff76 ";
+    var a=document.getElementById("by");
+    a.setAttribute("style", "color: #00ff76");
+    a.innerHTML=" ― Marilyn Monroe ";
+    document.body.style.backgroundColor = " #00ff76";
+    var b=document.getElementById("change");
+    b.setAttribute("style","background-color: #00ff76;");
+  }
+}
